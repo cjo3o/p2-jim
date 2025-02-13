17f03aab7d76ffe9dd1e66d9bc60c70c50efd2a2
cjo3o <cjy55973743@gmail.com>
2025-02-13 18:12:10 +0900

    예약페이지 레이아웃 완료, 조회페이지 레이아웃 작업 시작

---
+++ reserve.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const $sm = document.querySelector('#sm');
+    const $sp = document.querySelector('#sp');
+    const $mm = document.querySelector('#mm');
+    const $mp = document.querySelector('#mp');
+    const $lm = document.querySelector('#lm');
+    const $lp = document.querySelector('#lp');
+    const $small = document.querySelector('#small');
+    const $medium = document.querySelector('#medium');
+    const $large = document.querySelector('#large');
+
+    // //소형 수화물
+    // $sm.addEventListener('click', function () {
+    //
+    //     if ($small.value > 0) {
+    //         $small.value--;
+    //     }
+    // })
+    //
+    // $sp.addEventListener('click', function () {
+    //     $small.value++;
+    //     console.log()
+    // })
+    //
+    // //중형 수화물
+    // $mm.addEventListener('click', function () {
+    //     if ($medium.value > 0) {
+    //         $medium.value--;
+    //     }
+    // })
+    //
+    // $mp.addEventListener('click', function () {
+    //     $medium.value++;
+    //     console.log()
+    // })
+    //
+    // //대형 수화물
+    // $lm.addEventListener('click', function () {
+    //     if ($large.value > 0) {
+    //         $large.value--;
+    //     }
+    // })
+    //
+    // $lp.addEventListener('click', function () {
+    //     $large.value++;
+    //     console.log()
+    // })
+})
